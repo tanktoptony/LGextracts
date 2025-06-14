f42eb699f0fe45f15850927da09274fd84659a3b
tanktoptony <anthony.jarina@gmail.com>
2025-06-14 13:16:53 -0500

    messing around with styling - switch to lux

--- liquidgold/blog/urls.py
+++ liquidgold/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blog/", views.blog_index, name="blog_index"),
+    path("", views.blog_index, name="blog_index"), ### you can edit here to make a different page the home page
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
+    
 ]
